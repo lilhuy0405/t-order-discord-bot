32e363daee4a03c1632a23ae26f0ff9a85f6dede
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-03-01 17:16:23 +0700

    update more commands

---
+++ src/commands/Calculator.ts
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from "discord.js";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('calculator')
+        .setDescription('Simple calculator if you need it :v')
+        .addStringOption(option =>
+            option.setName('expression')
+                .setDescription('The expression you want to calculate')),
+    async execute(interaction: any) {
+        try {
+            const expression = interaction.options.getString('expression');
+            if (!expression) throw new Error('Please enter an expression');
+            const res = eval(expression);
+
+            await interaction.reply(`Em Huy quick math: ${expression} = ${res}`);
+        } catch (e) {
+            console.log(e);
+            await interaction.reply("Invalid expression");
+        }
+    },
+};
