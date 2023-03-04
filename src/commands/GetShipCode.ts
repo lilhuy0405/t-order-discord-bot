afa784d664a48196c40562aa275de1f25ce1afa3
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-03-04 23:15:44 +0700

    pagination

--- src/commands/GetShipCode.ts
+++ src/commands/GetShipCode.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 import Excel = require('exceljs');
 import { RequestInfo, RequestInit } from 'node-fetch';
 import { getWebsiteContent } from '../util';
@@ -51,6 +51,33 @@ const getTrackingStatus = async (shipCode: string) => {
     }
 }
 
+const getPaginateButton = (page: number, maxPage: number) => {
+    const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('previous_embed')
+                .setEmoji('⏮')
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(page === 0)
+        )
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('current_embed')
+                .setLabel(`Trang: ${page + 1}/${maxPage}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(true)
+        )
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('next_embed')
+                .setEmoji('⏭')
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(page === maxPage - 1)
+
+        );
+    return row;
+}
+
 const getShipCode = async (interaction: any) => {
     try {
         const apiURL = 'https://torder-api.click/orders/';
@@ -65,47 +92,43 @@ const getShipCode = async (interaction: any) => {
             await interaction.reply('Không tìm thấy mvd nào. Mua gì đi rồi mình nói chuyện tiếp');
             return;
         }
-
-        
-        
-        let listOrderMsg = await Promise.all(listOrders.map(async (order: any, i) => {
-            const contentItem = `**${i + 1}. MVD: ${order.shipCode}** | Tên người nhận: ${order.customerName} | Hàng: ${order.product}`
+        //from list orders build list embeds
+        const listEmbeds = await Promise.all(listOrders.map(async (order: any, i: number) => {
+            let description = '';
             const trackingStatus = await getTrackingStatus(order.shipCode);
-         
-            
-            return contentItem.concat(`\n`).concat(trackingStatus);
+            description += `**${i + 1}. MVD: ${order.shipCode}** | Tên người nhận: ${order.customerName} | Hàng: ${order.product}`
+            description += `\n`.concat(trackingStatus);
+            return {
+                color: 0x0099ff,
+                title: 'Danh sách mvd của ' + listOrders[0].customerName,
+                author: {
+                    name: '65% bot',
+                    icon_url: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+                },
+
+                description,
+                footer: {
+                    text: 'Bot created by lilhuy',
+                    icon_url: 'https://res.cloudinary.com/dfpf4gsti/image/upload/v1677298052/me_h2dzlt.jpg',
+                },
+            };
         }));
-        let msg = listOrderMsg.join('\n');
+        let currentEmbedIndex = 0;
 
+        //handle button click
+        const filter = (i: any) => i.customId === 'next_embed' || i.customId === 'previous_embed';
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 5 * 60 * 1000 });
+        collector.on('collect', async (i: any) => {
+            if (i.customId === 'next_embed') {
+                currentEmbedIndex++;
+            } else if (i.customId === 'previous_embed') {
+                currentEmbedIndex--;
+            }
+            await i.update({ embeds: [listEmbeds[currentEmbedIndex]], components: [getPaginateButton(currentEmbedIndex, listEmbeds.length)] });
 
-        const orderShipCodeQuery = listOrders.map((order: any) => {
-            return order.shipCode;
-        }).join(',');
+        });
 
-        const searchString = queryString.stringify({
-            type: 'track',
-            billcode: orderShipCodeQuery
-        })
-
-
-        msg += `\n\nTổng cộng: ${listOrders.length} mvd. Tra cứu vận đơn tại: https://jtexpress.vn/vi/tracking?${searchString}`;
-
-        const responseEmbeded = {
-            color: 0x0099ff,
-            title: 'Danh sách mvd của ' + listOrders[0].customerName,
-            author: {
-                name: '65% bot',
-                icon_url: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
-            },
-
-            description: msg,
-            footer: {
-                text: 'Bot created by lilhuy',
-                icon_url: 'https://res.cloudinary.com/dfpf4gsti/image/upload/v1677298052/me_h2dzlt.jpg',
-            },
-
-        };
-        await interaction.reply({ embeds: [responseEmbeded] });
+        await interaction.reply({ embeds: [listEmbeds[0]], components: [getPaginateButton(currentEmbedIndex, listEmbeds.length)] });
 
 
     } catch (err) {
