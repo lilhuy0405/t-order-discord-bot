f233e8673f13dcbed37a5810d77a750dbc74f2ee
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-07-03 10:13:34 +0700

    fix keyword

--- src/keywords/index.ts
+++ src/keywords/index.ts
@@ -3,25 +3,5 @@ export const keywords = [
     matches: ['mvd', 'mvđ', 'mã vận đơn'],
     reply: 'Các bạn vui lòng tải app IGHTK để tra cứu mã vận đơn nhé, nếu chưa có mã vận đơn vui lòng tạo ticket'
   },
-  {
-    matches: ['kha', 'kha lé'],
-    reply: 'Kha lé xem segg bị cả server biết, kha lé phờ ri nút'
-  },
-  {
-    matches: ['65%'],
-    reply: '65% is the best'
-  },
-  {
-    matches: ['bot ngu'],
-    reply: 'Bot ngu thì mày còn ngu hơn'
-  },
-  {
-    matches: ['bot ơi'],
-    reply: 'Gọi tao có việc gì không'
-  },
-  {
-    matches: ['lilhuy', 'huy'],
-    reply: 'Bot này là của lilhuy đó'
-  }
 
 ]
