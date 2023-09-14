b97dfb8fd92f72201a8a3d7fec41511a751ebf8c
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-09-14 17:45:29 +0700

    update api endpoint

--- src/commands/GetShipCode.ts
+++ src/commands/GetShipCode.ts
@@ -98,7 +98,7 @@ const getShipCode = async (interaction: any) => {
         //get sender
         await interaction.deferReply({ ephemeral: true });
 
-        const apiURL = 'https://torder-api.click/orders/';
+        const apiURL = 'https://torder-api.click/api/v1orders/';
         phone = interaction.options.getString('phone');
 
 
